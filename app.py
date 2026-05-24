dda174fce488294fcfeb7e5a0afe94b13eda3d8f
Phil Hevi <khevi@yahoo.com>
2026-05-23 21:53:25 -0400

    Add Jenkinsfile for Flask deployment

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def home():
         <div class="status">Database Connected Successfully ✅</div>
         <p>
             This Flask application is running inside Docker, connected to MySQL,
-            deployed through Jenkins, and managed with Docker Compose.
+            deployed through Jenkins, and managed with Docker Compose next will be k8.
         </p>
         <div class="badge">CI/CD Pipeline Active</div>
     </div>

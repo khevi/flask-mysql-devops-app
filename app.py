9c12cc86795d0801c970ec6e204bc0bf4d4928a9
Phil Hevi <khevi@yahoo.com>
2026-05-24 21:31:00 -0400

    Fix duplicate app.run

--- app.py
+++ app.py
@@ -184,9 +184,7 @@ def page_template(content):
     </body>
     </html>
     """
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+ 
 @app.route("/health")
 def health():
     try:
